59aa359f2e0e5795a200b0e7f7e35337cf1d2aee
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-27 09:04:57 -0300

    build(api): configure jest to resolve path aliases
    - Updates jest.config.js with a `moduleNameMapper` to handle TypeScript aliases.
    - This maps '@/*' to the 'src' directory and '@test/*' to the 'test' directory,
    ensuring modules are correctly resolved during tests.

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -4,6 +4,10 @@ const tsJestTransformCfg = createDefaultPreset().transform
 
 export default {
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
+  },
   transform: {
     ...tsJestTransformCfg,
   },
