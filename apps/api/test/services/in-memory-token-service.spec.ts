2dfc4e801dfd4dabc57a53d7121132f134101e71
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-30 14:37:03 -0300

    test(services): add unit tests for in-memory token service
    - Adds a comprehensive test suite for the `InMemoryTokenService`.
    - Tests cover saving, retrieving, deleting, and automatic expiration of tokens,
    ensuring the mock service behaves as expected.

---
+++ apps/api/test/services/in-memory-token-service.spec.ts
@@ -0,0 +1,90 @@
+import { randomUUID } from 'node:crypto'
+
+import { InMemoryTokenService } from './in-memory-token-service'
+
+let inMemoryTokenService: InMemoryTokenService
+
+describe('In Memory Token Service', () => {
+  beforeEach(() => {
+    inMemoryTokenService = new InMemoryTokenService()
+  })
+
+  it('should be able to save a token', () => {
+    const key = 'test:test-1'
+    const token = randomUUID()
+
+    const ttlSeconds = 10
+
+    const value = JSON.stringify({
+      id: 'test-1',
+      token,
+      expiresAt: new Date(Date.now() + ttlSeconds * 1000).toISOString(),
+    })
+
+    inMemoryTokenService.save(key, value, ttlSeconds)
+
+    expect(inMemoryTokenService.items.size).toEqual(1)
+
+    const valueData = JSON.parse(
+      inMemoryTokenService.items.get(key)?.value ?? '',
+    )
+    expect(valueData.id).toEqual('test-1')
+  })
+
+  it('should be able to get a saved token', async () => {
+    const key = 'test:test-1'
+    const token = randomUUID()
+
+    const ttlSeconds = 10
+
+    const value = JSON.stringify({
+      id: 'test-1',
+      token,
+      expiresAt: new Date(Date.now() + ttlSeconds * 1000).toISOString(),
+    })
+
+    inMemoryTokenService.save(key, value, ttlSeconds)
+
+    const data = await inMemoryTokenService.get(key)
+    expect(data).not.toBeNull()
+    const valueData = JSON.parse(data ?? '')
+    expect(valueData.id).toEqual('test-1')
+  })
+
+  it('should be able to delete a saved token', async () => {
+    const key = 'test:test-1'
+    const token = randomUUID()
+
+    const ttlSeconds = 10
+
+    const value = JSON.stringify({
+      id: 'test-1',
+      token,
+      expiresAt: new Date(Date.now() + ttlSeconds * 1000).toISOString(),
+    })
+
+    inMemoryTokenService.save(key, value, ttlSeconds)
+    inMemoryTokenService.delete(key)
+
+    expect(inMemoryTokenService.items.size).toEqual(0)
+  })
+
+  it('should be able to delete a expired token automatically', async () => {
+    const key = 'test:test-1'
+    const token = randomUUID()
+
+    const ttlSeconds = 1
+
+    const value = JSON.stringify({
+      id: 'test-1',
+      token,
+      expiresAt: new Date(Date.now() + ttlSeconds * 1000).toISOString(),
+    })
+
+    inMemoryTokenService.save(key, value, ttlSeconds)
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    inMemoryTokenService.get(key)
+
+    expect(inMemoryTokenService.items.size).toEqual(0)
+  })
+})
