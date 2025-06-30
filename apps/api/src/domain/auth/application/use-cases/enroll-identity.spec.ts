ab9240d7922ca5f91474688a3c692efb75d4aad6
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-30 08:32:26 -0300

    test(application): add unit tests for enroll identity
    - Adds a test suite for `EnrollIdentityUseCase`.
    - The tests cover successful registration, handling of duplicate emails,
    and automatic password hashing, ensuring the core logic is robust and correct.

---
+++ apps/api/src/domain/auth/application/use-cases/enroll-identity.spec.ts
@@ -0,0 +1,64 @@
+import { faker } from '@faker-js/faker'
+import { makeUser } from '@test/factories/make-user'
+import { InMemoryUsersRepository } from '@test/repositories/in-memory-users-repository'
+
+import { EnrollIdentityUseCase } from './enroll-identity'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let sut: EnrollIdentityUseCase
+
+describe('Enroll Identity Use-case', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    sut = new EnrollIdentityUseCase(inMemoryUsersRepository)
+  })
+
+  it('should be able to enroll identity', async () => {
+    const response = await sut.execute({
+      name: 'Name Test',
+      email: 'example@email.com',
+      password: '123456',
+      avatarUrl: faker.image.avatar(),
+    })
+
+    expect(response.isRight()).toBeTruthy()
+
+    if (response.isRight()) {
+      const { user } = response.value
+      expect(user.email).toEqual('example@email.com')
+      expect(inMemoryUsersRepository.items[0].id).toEqual(user.id)
+    }
+  })
+
+  it('should not be able to enroll identity with an email that is already in use', async () => {
+    const user = await makeUser({ email: 'example@email.com' })
+    inMemoryUsersRepository.items.push(user)
+
+    const response = await sut.execute({
+      name: 'Name Test',
+      email: 'example@email.com',
+      password: '123456',
+    })
+
+    expect(response.isLeft()).toBeTruthy()
+    expect(response.value).toBeInstanceOf(Error)
+    expect(response.value).toHaveProperty(
+      'message',
+      'Este e-mail já está em uso.',
+    )
+  })
+
+  it('should be able to hash password automatically', async () => {
+    const response = await sut.execute({
+      name: 'Name Test',
+      email: 'example@email.com',
+      password: '123456',
+    })
+
+    if (response.isRight()) {
+      const { user } = response.value
+      expect(user.passwordHash).not.toEqual('123456')
+      expect(await user.verifyPassword('123456')).toBeTruthy()
+    }
+  })
+})
