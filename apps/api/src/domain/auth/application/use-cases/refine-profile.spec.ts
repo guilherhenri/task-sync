f655d7a880c214d7fd51215d6723fff87fe547ea
Guilherme Henrique <guilherhenri12@gmail.com>
2025-07-01 07:48:43 -0300

    test(application): add unit tests for refine profile use case
    - Adds a test suite for `RefineProfileUseCase`.
    - The tests cover successful profile updates (with and without a new password) and
    failure cases like updating a non-existent user or using a duplicate email.

---
+++ apps/api/src/domain/auth/application/use-cases/refine-profile.spec.ts
@@ -0,0 +1,110 @@
+import { makeUser } from '@test/factories/make-user'
+import { InMemoryUsersRepository } from '@test/repositories/in-memory-users-repository'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+import { RefineProfileUseCase } from './refine-profile'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let sut: RefineProfileUseCase
+
+describe('Refine Profile Use-case', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    sut = new RefineProfileUseCase(inMemoryUsersRepository)
+  })
+
+  it('should be able to update a valid user', async () => {
+    const user = await makeUser({
+      name: 'Test Name',
+      email: 'example@email.com',
+      password: '123456',
+    })
+    inMemoryUsersRepository.items.push(user)
+
+    const response = await sut.execute({
+      userId: user.id.toString(),
+      name: 'Updated Name',
+      email: 'updated@email.com',
+      newPassword: '654321',
+    })
+
+    expect(response.isRight()).toBeTruthy()
+    expect(inMemoryUsersRepository.items[0]).toEqual(
+      expect.objectContaining({
+        name: 'Updated Name',
+        email: 'updated@email.com',
+      }),
+    )
+    expect(
+      await inMemoryUsersRepository.items[0].verifyPassword('654321'),
+    ).toBeTruthy()
+  })
+
+  it('should be able to update a user without change the password', async () => {
+    const user = await makeUser({
+      name: 'Test Name',
+      email: 'example@email.com',
+      password: '123456',
+    })
+    inMemoryUsersRepository.items.push(user)
+
+    const response = await sut.execute({
+      userId: user.id.toString(),
+      name: 'Updated Name',
+      email: 'updated@email.com',
+    })
+
+    expect(response.isRight()).toBeTruthy()
+    expect(inMemoryUsersRepository.items[0]).toEqual(
+      expect.objectContaining({
+        name: 'Updated Name',
+        email: 'updated@email.com',
+      }),
+    )
+    expect(
+      await inMemoryUsersRepository.items[0].verifyPassword('123456'),
+    ).toBeTruthy()
+  })
+
+  it('should not be able to update an invalid user', async () => {
+    const response = await sut.execute({
+      userId: 'user-1',
+      name: 'Updated Name',
+      email: 'updated@email.com',
+    })
+
+    expect(response.isLeft()).toBeTruthy()
+    expect(response.value).toHaveProperty('message', 'Usuário não encontrado.')
+  })
+
+  it('should not be able to update a user with an email that is already in use', async () => {
+    const user1 = await makeUser(
+      {
+        email: 'user1@email.com',
+      },
+      new UniqueEntityID('user-1'),
+    )
+    const user2 = await makeUser(
+      {
+        email: 'user2@email.com',
+      },
+      new UniqueEntityID('user-2'),
+    )
+    inMemoryUsersRepository.items.push(user1)
+    inMemoryUsersRepository.items.push(user2)
+
+    const response = await sut.execute({
+      userId: 'user-1',
+      name: 'Updated Name',
+      email: 'user2@email.com',
+    })
+
+    expect(response.isLeft()).toBeTruthy()
+    expect(response.value).toHaveProperty(
+      'message',
+      'Este e-mail já está em uso.',
+    )
+    expect(inMemoryUsersRepository.items[0].email).toEqual('user1@email.com')
+  })
+})
