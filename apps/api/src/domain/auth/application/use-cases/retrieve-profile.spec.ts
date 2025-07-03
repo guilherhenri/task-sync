4a8e1b268c7dcde6bd62d2456a7e61f047d58191
Guilherme Henrique <guilherhenri12@gmail.com>
2025-07-03 09:54:04 -0300

    test(application): add unit tests for retrieve profile
    - Adds a test suite for `RetrieveProfileUseCase`.
    - The tests cover the successful retrieval of an existing user's profile and
    the failure case when the user ID does not exist, ensuring correct data fetching.

---
+++ apps/api/src/domain/auth/application/use-cases/retrieve-profile.spec.ts
@@ -0,0 +1,45 @@
+import { makeUser } from '@test/factories/make-user'
+import { InMemoryUsersRepository } from '@test/repositories/in-memory-users-repository'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+import { RetrieveProfileUseCase } from './retrieve-profile'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let sut: RetrieveProfileUseCase
+
+describe('Retrieve Profile Use-case', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    sut = new RetrieveProfileUseCase(inMemoryUsersRepository)
+  })
+
+  it('should be able to retrieve a profile for a valid user id', async () => {
+    const user = await makeUser(
+      { name: 'User Test', email: 'example@email.com' },
+      new UniqueEntityID('user-id'),
+    )
+    inMemoryUsersRepository.items.push(user)
+
+    const response = await sut.execute({ userId: 'user-id' })
+
+    expect(response.isRight()).toBeTruthy()
+
+    if (response.isRight()) {
+      const { user } = response.value
+      expect(user).toEqual(
+        expect.objectContaining({
+          name: 'User Test',
+          email: 'example@email.com',
+        }),
+      )
+    }
+  })
+
+  it('should not be able to retrieve a  profile for a user does not exist', async () => {
+    const response = await sut.execute({ userId: 'user-id' })
+
+    expect(response.isLeft()).toBeTruthy()
+    expect(response.value).toHaveProperty('message', 'Usuário não encontrado.')
+  })
+})
