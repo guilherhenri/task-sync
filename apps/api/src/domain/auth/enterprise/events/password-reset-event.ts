bc92cdade26a527bb767dabc364a4366d686e88c
Guilherme Henrique <guilherhenri12@gmail.com>
2025-07-05 13:37:26 -0300

    refactor(domain): update password reset event to use user aggregate
    - Refactors the `PasswordResetEvent` to be associated directly with the `User`
    aggregate instead of a `VerificationToken`.
    - This aligns the event with the new `resetPassword` method on the `User` entity.

--- apps/api/src/domain/auth/enterprise/events/password-reset-event.ts
+++ apps/api/src/domain/auth/enterprise/events/password-reset-event.ts
@@ -1,18 +1,18 @@
 import type { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import type { DomainEvent } from '@/core/events/domain-event'
 
-import type { VerificationToken } from '../entities/verification-token'
+import type { User } from '../entities/user'
 
 export class PasswordResetEvent implements DomainEvent {
   public ocurredAt: Date
-  public verificationToken: VerificationToken
+  public user: User
 
-  constructor(verificationToken: VerificationToken) {
-    this.verificationToken = verificationToken
+  constructor(user: User) {
+    this.user = user
     this.ocurredAt = new Date()
   }
 
   getAggregateId(): UniqueEntityID {
-    return this.verificationToken.id
+    return this.user.id
   }
 }
