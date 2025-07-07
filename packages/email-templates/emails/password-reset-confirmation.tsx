4776e067e1d78c8d9b38e3a3f252468b208fc0b6
Guilherme Henrique <guilherhenri12@gmail.com>
2025-07-07 16:33:25 -0300

    feat(email): add password reset confirmation template
    - Creates the `PasswordResetConfirmationEmail` template.
    - This email serves as a notification to the user that their password has been successfully
    changed, enhancing account security.

---
+++ packages/email-templates/emails/password-reset-confirmation.tsx
@@ -0,0 +1,55 @@
+import { Text } from '@react-email/components'
+import * as React from 'react'
+
+import { Content } from '../components/content'
+import { Footer } from '../components/footer'
+import { Header } from '../components/header'
+import { Main } from '../components/main'
+import { Title } from '../components/title'
+import { colors } from '../constants'
+
+export interface PasswordResetConfirmationEmailProps {
+  name: string
+}
+
+export const PasswordResetConfirmationEmail = ({
+  name,
+}: PasswordResetConfirmationEmailProps) => (
+  <Main>
+    <Header />
+
+    <Content>
+      <Title text={`Oi, ${name}!`} />
+      <Text
+        style={{
+          color: colors.foreground,
+          fontSize: '16px',
+          lineHeight: '24px',
+          marginBottom: '20px',
+        }}
+      >
+        Sua senha foi redefinida com sucesso! Agora você pode fazer login na
+        nossa plataforma usando sua nova senha.
+      </Text>
+      <Text
+        style={{
+          color: colors.foreground,
+          fontSize: '16px',
+          lineHeight: '24px',
+          marginBottom: '20px',
+        }}
+      >
+        Se você não realizou essa alteração, por favor, entre em contato com
+        nosso suporte imediatamente.
+      </Text>
+    </Content>
+
+    <Footer />
+  </Main>
+)
+
+PasswordResetConfirmationEmail.PreviewProps = {
+  name: 'Guilherme Henrique',
+} as PasswordResetConfirmationEmailProps
+
+export default PasswordResetConfirmationEmail
