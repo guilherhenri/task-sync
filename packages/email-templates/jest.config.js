fd88be0a28acd23c966ed510b382e1000c854ff1
Guilherme Henrique <guilherhenri12@gmail.com>
2025-07-07 16:25:35 -0300

    build(email): configure jest for typescript and esm
    - Creates a `jest.config.js` file to configure the test environment.
    - It sets up `ts-jest` to transform TypeScript files and enables ESM support, aligning the
    test runner with the package's module system.

---
+++ packages/email-templates/jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        useESM: true,
+      },
+    ],
+  },
+}
