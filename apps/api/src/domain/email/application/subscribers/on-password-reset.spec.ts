fa15a724c948d5c3143b8ca7b9c918d82a7ec71e
Guilherme Henrique <guilherhenri12@gmail.com>
2025-07-09 15:47:41 -0300

    test(application): add tests for password reset subscriber
    - Adds a test suite for the `OnPasswordRest` subscriber.
    - The test ensures that after a user's password is reset and saved, the handler is triggered
    to create a confirmation email request.

---
+++ apps/api/src/domain/email/application/subscribers/on-password-reset.spec.ts
@@ -0,0 +1,55 @@
+import type { EmailTemplateType } from '@task-sync/api-types'
+import { makeUser } from '@test/factories/make-user'
+import { InMemoryEmailRequestsRepository } from '@test/repositories/in-memory-email-requests-repository'
+import { InMemoryUsersRepository } from '@test/repositories/in-memory-users-repository'
+import { InMemoryAuthUserService } from '@test/services/in-memory-auth-user-service'
+import { InMemoryEmailQueueService } from '@test/services/in-memory-email-queue-service'
+import { waitFor } from '@test/utils/wait-for'
+
+import {
+  CreateEmailRequestUseCase,
+  type CreateEmailRequestUseCaseRequest,
+  type CreateEmailRequestUseCaseResponse,
+} from '../use-cases/create-email-request'
+import { OnPasswordRest } from './on-password-reset'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let inMemoryAuthUserService: InMemoryAuthUserService
+let inMemoryEmailRequestsRepository: InMemoryEmailRequestsRepository
+let inMemoryEmailQueueService: InMemoryEmailQueueService
+let sut: CreateEmailRequestUseCase
+
+let createEmailRequestExecuteSpy: jest.SpyInstance<
+  Promise<CreateEmailRequestUseCaseResponse>,
+  [CreateEmailRequestUseCaseRequest<EmailTemplateType>]
+>
+
+describe('On Password Reset', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    inMemoryAuthUserService = new InMemoryAuthUserService(
+      inMemoryUsersRepository,
+    )
+    inMemoryEmailRequestsRepository = new InMemoryEmailRequestsRepository()
+    inMemoryEmailQueueService = new InMemoryEmailQueueService()
+    sut = new CreateEmailRequestUseCase(
+      inMemoryEmailRequestsRepository,
+      inMemoryEmailQueueService,
+    )
+
+    createEmailRequestExecuteSpy = jest.spyOn(sut, 'execute')
+
+    new OnPasswordRest(inMemoryAuthUserService, sut)
+  })
+
+  it('should add an email request to the queue when a verify token is registered', async () => {
+    const user = await makeUser()
+    inMemoryUsersRepository.items.push(user)
+
+    await user.resetPassword('123456')
+
+    inMemoryUsersRepository.save(user)
+
+    await waitFor(() => expect(createEmailRequestExecuteSpy).toHaveBeenCalled())
+  })
+})
