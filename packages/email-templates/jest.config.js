7e5595a55657b63247ab93c61c871ce90cc0ef54
Guilherme Henrique <guilherhenri12@gmail.com>
2025-07-23 13:50:42 -0300

    build(email): update jest config to ignore build outputs
    - Adjusts the Jest configuration for the email templates package to ignore the
    `dist` and `.react-email` directories.
    This prevents the test runner from unnecessarily scanning build artifacts and
    cache folders.

--- packages/email-templates/jest.config.js
+++ packages/email-templates/jest.config.js
@@ -1,5 +1,6 @@
 export default {
   testEnvironment: 'jsdom',
+  modulePathIgnorePatterns: ['<rootDir>/.react-email/', '<rootDir>/dist/'],
   transform: {
     '^.+\\.(ts|tsx)$': [
       'ts-jest',
