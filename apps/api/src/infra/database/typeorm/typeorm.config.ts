6c6a18ad03043ed4d49c9884e51f35288da47acb
Guilherme Henrique <guilherhenri12@gmail.com>
2025-07-24 12:40:23 -0300

    feat(infra): create centralized typeorm configuration
    - Implements `typeorm.config.ts` to provide a centralized configuration for
    TypeORM.
    - It reads database credentials from the `EnvService` and defines paths
    for entities, migrations, and seeds, with environment-specific settings.

---
+++ apps/api/src/infra/database/typeorm/typeorm.config.ts
@@ -0,0 +1,29 @@
+import path from 'node:path'
+
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+import type { SeederOptions } from 'typeorm-extension'
+
+import type { EnvService } from '../../env/env.service'
+
+export const migrationsDir = path.resolve(__dirname, 'migrations')
+
+export const typeOrmConfig = (
+  config: EnvService,
+): TypeOrmModuleOptions & SeederOptions => ({
+  type: 'postgres',
+  host: config.get('POSTGRES_HOST'),
+  port: config.get('POSTGRES_PORT'),
+  username: config.get('POSTGRES_USER'),
+  password: config.get('POSTGRES_PASSWORD'),
+  database: config.get('POSTGRES_DB'),
+  entities: [path.resolve(__dirname, 'entities', '*.entity{.ts,.js}')],
+  migrations: [`${migrationsDir}/*{.ts,.js}`],
+  seeds: [path.resolve(__dirname, 'seeds', '*.seeder{.ts,.js}')],
+  seedTracking: false,
+  factories: [path.resolve(__dirname, 'factories', '*.factory{.ts,.js}')],
+  synchronize: config.get('NODE_ENV') === 'development',
+  migrationsRun: config.get('NODE_ENV') === 'development',
+  logging: config.get('NODE_ENV') === 'development' ? ['error', 'warn'] : false,
+})
+
+export default typeOrmConfig
