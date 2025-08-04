c88873a1d725f35b72a182b4118b41405a695350
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-04 16:14:32 -0300

    build(api): configure jest for end-to-end tests
    - Creates `jest.e2e.config.ts` to set up the environment for end-to-end testing.
    - It specifies the test file pattern (`.e2e-spec.ts`) and a setup file for E2E-specific
    configurations.

---
+++ apps/api/jest.e2e.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest'
+import { createDefaultPreset } from 'ts-jest'
+
+const tsJestTransformCfg = createDefaultPreset().transform
+
+export default async (): Promise<Config> => ({
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
+    '^@task-sync/email-templates$':
+      '<rootDir>/test/mocks/@task-sync/email-templates.ts',
+  },
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  testMatch: ['**/*.e2e-spec.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test/setup-e2e.ts'],
+})
