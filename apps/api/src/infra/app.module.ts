61c81b264e3b35bd28f38a43f2279317c3a40600
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-04 16:52:24 -0300

    feat(infra): add events and workers modules to app module
    - Integrates the `EventsModule` and `WorkersModule` into the root `AppModule`.
    - This sets up the necessary infrastructure for handling domain event
    subscriptions and background job processing.

--- apps/api/src/infra/app.module.ts
+++ apps/api/src/infra/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import { EnvModule } from './env/env.module'
+import { EventsModule } from './events/events.module'
 import { HttpModule } from './http/http.module'
+import { WorkersModule } from './workers/workers.module'
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import { HttpModule } from './http/http.module'
     }),
     HttpModule,
     EnvModule,
+    EventsModule,
+    WorkersModule,
   ],
 })
 export class AppModule {}
