cbbd8d05ac965ac5351503e3c41a475694d6780d
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-04 16:53:38 -0300

    refactor(infra): configure database module with repository providers
    - Updates the `DatabaseModule` to provide concrete repository implementations
    (`TypeOrmUsersRepository`, `MongooseEmailRequestsRepository`) for their
    respective abstract repository tokens, connecting the domain to the persistence layer.

--- apps/api/src/infra/database/database.module.ts
+++ apps/api/src/infra/database/database.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { UsersRepository } from '@/domain/auth/application/repositories/users-repository'
+import { EmailRequestsRepository } from '@/domain/email/application/repositories/email-requests-repository'
 
 import { EnvModule } from '../env/env.module'
 import { EnvService } from '../env/env.service'
+import { mongooseConfig } from './mongoose/mongoose.config'
+import { MongooseService } from './mongoose/mongoose.service'
+import { MongooseEmailRequestsRepository } from './mongoose/repositories/mongoose-email-requests-repository'
 import { TypeOrmUsersRepository } from './typeorm/repositories/typeorm-users-repository'
 import typeOrmConfig from './typeorm/typeorm.config'
 import { TypeOrmService } from './typeorm/typeorm.service'
@@ -16,14 +21,29 @@ import { TypeOrmService } from './typeorm/typeorm.service'
       useFactory: typeOrmConfig,
       inject: [EnvService],
     }),
+    MongooseModule.forRootAsync({
+      imports: [EnvModule],
+      useFactory: mongooseConfig,
+      inject: [EnvService],
+    }),
   ],
   providers: [
     TypeOrmService,
+    MongooseService,
     {
       provide: UsersRepository,
       useClass: TypeOrmUsersRepository,
     },
+    {
+      provide: EmailRequestsRepository,
+      useClass: MongooseEmailRequestsRepository,
+    },
   ],
-  exports: [TypeOrmService, UsersRepository],
+  exports: [
+    TypeOrmService,
+    MongooseService,
+    UsersRepository,
+    EmailRequestsRepository,
+  ],
 })
 export class DatabaseModule {}
