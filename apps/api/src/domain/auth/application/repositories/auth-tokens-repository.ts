9aac91137b5fd07f67343e936209054f069b2b2a
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-04 16:20:34 -0300

    refactor(application): convert auth tokens repository to abstract class
    - Transforms the `AuthTokensRepository` from an interface to an abstract class.
    - This change is necessary to allow it to be used as an injection token within
    NestJS's dependency injection system.

--- apps/api/src/domain/auth/application/repositories/auth-tokens-repository.ts
+++ apps/api/src/domain/auth/application/repositories/auth-tokens-repository.ts
@@ -1,8 +1,8 @@
 import type { AuthToken } from '../../enterprise/entities/auth-token'
 
-export interface AuthTokensRepository {
-  findByRefreshToken(refreshToken: string): Promise<AuthToken | null>
+export abstract class AuthTokensRepository {
+  abstract findByRefreshToken(refreshToken: string): Promise<AuthToken | null>
 
-  create(authToken: AuthToken): Promise<void>
-  delete(authToken: AuthToken): Promise<void>
+  abstract create(authToken: AuthToken): Promise<void>
+  abstract delete(authToken: AuthToken): Promise<void>
 }
