5035d7b82b3c4696bde64d63c75c4a003a31fb0d
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-04 17:16:36 -0300

    test(e2e): add test for welcome email on registration
    - Creates an E2E test to confirm that a welcome email request is created and
    processed after a new user signs up.
    - This validates the `OnUserRegistered` subscriber and its integration with
    the entire application stack.

---
+++ apps/api/src/infra/events/on-user-registered.e2e-spec.ts
@@ -0,0 +1,58 @@
+import type { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { waitFor } from '@test/utils/wait-for'
+import type { Model } from 'mongoose'
+import request from 'supertest'
+
+import { DomainEvents } from '@/core/events/domain-events'
+import { AppModule } from '@/infra/app.module'
+
+import { MongooseService } from '../database/mongoose/mongoose.service'
+import {
+  type EmailRequest as MongooseEmailRequest,
+  EmailRequestSchema,
+} from '../database/mongoose/schemas/email-request.schema'
+
+describe('On user registered (E2E)', () => {
+  let app: INestApplication
+  let mongoose: MongooseService
+  let emailRequestModel: Model<MongooseEmailRequest>
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    mongoose = moduleRef.get(MongooseService)
+    emailRequestModel = mongoose.connection.model<MongooseEmailRequest>(
+      'EmailRequest',
+      EmailRequestSchema,
+    )
+
+    DomainEvents.shouldRun = true
+
+    await app.init()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should send a email when user is registered', async () => {
+    await request(app.getHttpServer()).post('/sign-up').send({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '12345Ab@',
+    })
+    await waitFor(async () => {
+      const emailRequestOnDatabase = await emailRequestModel.findOne({
+        recipientEmail: 'johndoe@email.com',
+        eventType: 'user_registered',
+        status: 'sent',
+      })
+      expect(emailRequestOnDatabase).not.toBeNull()
+    }, 5000)
+  })
+})
