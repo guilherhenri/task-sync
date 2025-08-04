e699c913563a23cb869726b4fa30089c27db94a1
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-04 17:21:49 -0300

    feat(infra): update http module to include register controller
    - Updates the `HttpModule` to register the new `RegisterController` and provide
    the `EnrollIdentityUseCase`.
    - It also imports the `CryptographyModule` to make the `Hasher` service available
    for injection.

--- apps/api/src/infra/http/http.module.ts
+++ apps/api/src/infra/http/http.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common'
 
+import { EnrollIdentityUseCase } from '@/domain/auth/application/use-cases/enroll-identity'
+
+import { CryptographyModule } from '../cryptography/cryptography.module'
 import { DatabaseModule } from '../database/database.module'
+import { KeyValueModule } from '../key-value/key-value.module'
+import { RegisterController } from './controllers/register.controller'
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [],
+  imports: [DatabaseModule, KeyValueModule, CryptographyModule],
+  controllers: [RegisterController],
+  providers: [EnrollIdentityUseCase],
 })
 export class HttpModule {}
