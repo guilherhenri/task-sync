eede1fb08d40e68c634cd7f12971d9888c183562
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-05 07:01:42 -0300

    test(factories): create user factory for e2e tests
    - Creates a `UserFactory` injectable service for E2E tests.
    - This factory can create and persist a `User` directly to the database,
    simplifying the setup phase (Arrange) of end-to-end test scenarios.

--- apps/api/test/factories/make-user.ts
+++ apps/api/test/factories/make-user.ts
@@ -1,24 +1,21 @@
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { User, type UserProps } from '@/domain/auth/enterprise/entities/user'
+import { User as TypeOrmUser } from '@/infra/database/typeorm/entities/user.entity'
+import { TypeOrmUserMapper } from '@/infra/database/typeorm/mappers/typeorm-user-mapper'
+import { TypeOrmService } from '@/infra/database/typeorm/typeorm.service'
 
-export async function makeUser(
-  override: Partial<
-    Omit<
-      UserProps & {
-        password: string
-      },
-      'passwordHash'
-    >
-  > = {},
+export function makeUser(
+  override: Partial<UserProps> = {},
   id?: UniqueEntityID,
 ) {
-  const user = await User.create(
+  const user = User.create(
     {
       name: faker.person.fullName(),
       email: faker.internet.email(),
-      password: '123456',
+      passwordHash: faker.internet.password(),
       avatarUrl: faker.image.avatar(),
       ...override,
     },
@@ -26,4 +23,19 @@ export async function makeUser(
   )
 
   return user
+}
+
+@Injectable()
+export class UserFactory {
+  constructor(private readonly typeorm: TypeOrmService) {}
+
+  async makeTypeOrmUser(data: Partial<UserProps> = {}): Promise<User> {
+    const user = makeUser(data)
+
+    await this.typeorm
+      .getRepository(TypeOrmUser)
+      .save(TypeOrmUserMapper.toTypeOrm(user))
+
+    return user
+  }
 }
