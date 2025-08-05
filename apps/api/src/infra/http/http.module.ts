793a2eef8370ff478f24e4841c96a655ebbcbfde
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-05 13:17:34 -0300

    refactor(infra): register confirm email controller and use case
    - Updates the `HttpModule` to include the `ConfirmEmailController` in its list of
    controllers and provides the `ConfirmEmailUseCase`, making the new endpoint
    and its logic available to the application.

--- apps/api/src/infra/http/http.module.ts
+++ apps/api/src/infra/http/http.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common'
 
+import { ConfirmEmailUseCase } from '@/domain/auth/application/use-cases/confirm-email'
 import { EnrollIdentityUseCase } from '@/domain/auth/application/use-cases/enroll-identity'
 
 import { CryptographyModule } from '../cryptography/cryptography.module'
 import { DatabaseModule } from '../database/database.module'
 import { KeyValueModule } from '../key-value/key-value.module'
+import { ConfirmEmailController } from './controllers/confirm-email.controller'
 import { RegisterController } from './controllers/register.controller'
 
 @Module({
   imports: [DatabaseModule, KeyValueModule, CryptographyModule],
-  controllers: [RegisterController],
-  providers: [EnrollIdentityUseCase],
+  controllers: [RegisterController, ConfirmEmailController],
+  providers: [EnrollIdentityUseCase, ConfirmEmailUseCase],
 })
 export class HttpModule {}
