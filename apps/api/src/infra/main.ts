617a579f84a32012db1651deb85e7b02cafdc033
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-05 06:51:31 -0300

    feat(infra): configure swagger and global prefix in main bootstrap
    - Updates the main application bootstrap file (`main.ts`) to:
      . Set up Swagger/OpenAPI documentation at `/api/docs`.
      . Apply a global URL prefix of `/api/v1` to all routes.
      . Fetch the server port from the `EnvService`.

--- apps/api/src/infra/main.ts
+++ apps/api/src/infra/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3333)
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('TaskSync API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, configSwagger)
+  SwaggerModule.setup('api/docs', app, document)
+
+  app.setGlobalPrefix('/api/v1')
+
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
+
+  await app.listen(port)
 }
 bootstrap()
