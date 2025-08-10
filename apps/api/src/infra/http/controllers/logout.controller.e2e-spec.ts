5487de7a88b96befdb1ad22f2ee1a92de21a500c
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-10 07:58:29 -0300

    test(e2e): add test for logout endpoint
    - Creates an E2E test for the `DELETE /sessions` endpoint.
    - It uses a new `AuthenticateUserFactory` to simulate a logged-in user
    and verifies that the logout request is successful.

---
+++ apps/api/src/infra/http/controllers/logout.controller.e2e-spec.ts
@@ -0,0 +1,38 @@
+import type { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import { AuthenticateUserFactory } from '@test/factories/make-user'
+import { AuthTestModule } from '@test/modules/auth-test.module'
+import request from 'supertest'
+
+describe('Logout (E2E)', () => {
+  let app: INestApplication
+  let authenticateUserFactory: AuthenticateUserFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AuthTestModule],
+      providers: [AuthenticateUserFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    authenticateUserFactory = moduleRef.get(AuthenticateUserFactory)
+
+    await app.init()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('[DELETE] /sessions', async () => {
+    const { signedCookie } =
+      await authenticateUserFactory.makeAuthenticatedUser()
+
+    const response = await request(app.getHttpServer())
+      .delete('/sessions')
+      .set('Cookie', signedCookie)
+
+    expect(response.statusCode).toBe(200)
+  })
+})
