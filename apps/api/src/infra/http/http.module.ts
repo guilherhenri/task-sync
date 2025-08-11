ee21bdde094f552b218412224d9feb6d8df115ce
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-11 15:24:59 -0300

    refactor(infra): register forgot password controller and use case
    - Updates the `HttpModule` to provide the `InitiatePasswordRecoveryUseCase` and
    register the `ForgotPasswordController`.
    - This makes the `forgot password` functionality available via a public API endpoint.

--- apps/api/src/infra/http/http.module.ts
+++ apps/api/src/infra/http/http.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common'
 import { AuthenticateSessionUseCase } from '@/domain/auth/application/use-cases/authenticate-session'
 import { ConfirmEmailUseCase } from '@/domain/auth/application/use-cases/confirm-email'
 import { EnrollIdentityUseCase } from '@/domain/auth/application/use-cases/enroll-identity'
+import { InitiatePasswordRecoveryUseCase } from '@/domain/auth/application/use-cases/initiate-password-recovery'
 import { RenewTokenUseCase } from '@/domain/auth/application/use-cases/renew-token'
 import { TerminateSessionUseCase } from '@/domain/auth/application/use-cases/terminate-session'
 
@@ -12,6 +13,7 @@ import { EnvModule } from '../env/env.module'
 import { KeyValueModule } from '../key-value/key-value.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { ConfirmEmailController } from './controllers/confirm-email.controller'
+import { ForgotPasswordController } from './controllers/forgot-password.controller'
 import { LogoutController } from './controllers/logout.controller'
 import { RefreshTokenController } from './controllers/refresh-token.controller'
 import { RegisterController } from './controllers/register.controller'
@@ -24,6 +26,7 @@ import { RegisterController } from './controllers/register.controller'
     AuthenticateController,
     RefreshTokenController,
     LogoutController,
+    ForgotPasswordController,
   ],
   providers: [
     EnrollIdentityUseCase,
@@ -31,6 +34,7 @@ import { RegisterController } from './controllers/register.controller'
     AuthenticateSessionUseCase,
     RenewTokenUseCase,
     TerminateSessionUseCase,
+    InitiatePasswordRecoveryUseCase,
   ],
 })
 export class HttpModule {}
