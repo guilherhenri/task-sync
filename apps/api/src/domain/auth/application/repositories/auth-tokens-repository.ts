eab42c48040a284742359d59086f3ac2afcad6cd
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-13 09:06:33 -0300

    refactor(application): add revoke method to auth tokens repository
    - Extends the `AuthTokensRepository` abstract class to include a
    `revokeTokensByUserId` method.
    - This is necessary for a comprehensive session revocation, invalidating both
    short-lived and long-lived tokens.

--- apps/api/src/domain/auth/application/repositories/auth-tokens-repository.ts
+++ apps/api/src/domain/auth/application/repositories/auth-tokens-repository.ts
@@ -2,7 +2,7 @@ import type { AuthToken } from '../../enterprise/entities/auth-token'
 
 export abstract class AuthTokensRepository {
   abstract findByUserId(userId: string): Promise<AuthToken | null>
-
   abstract create(authToken: AuthToken): Promise<void>
   abstract delete(authToken: AuthToken): Promise<void>
+  abstract revokeTokensByUserId(userId: string): Promise<void>
 }
