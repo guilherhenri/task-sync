9eb2a7650591f06406783752c993ab464e6c212b
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-13 09:13:12 -0300

    refactor(infra): register get profile and revoke sessions controllers
    - Updates the `HttpModule` to provide the `RetrieveProfileUseCase` and
    `RevokeTokensUseCase`, and registers their corresponding controllers,
    `GetProfileController` and `RevokeAllSessionsController`.

--- apps/api/src/infra/http/http.module.ts
+++ apps/api/src/infra/http/http.module.ts
@@ -6,6 +6,8 @@ import { EnrollIdentityUseCase } from '@/domain/auth/application/use-cases/enrol
 import { InitiatePasswordRecoveryUseCase } from '@/domain/auth/application/use-cases/initiate-password-recovery'
 import { RenewTokenUseCase } from '@/domain/auth/application/use-cases/renew-token'
 import { ResetPasswordUseCase } from '@/domain/auth/application/use-cases/reset-password'
+import { RetrieveProfileUseCase } from '@/domain/auth/application/use-cases/retrieve-profile'
+import { RevokeTokensUseCase } from '@/domain/auth/application/use-cases/revoke-tokens'
 import { TerminateSessionUseCase } from '@/domain/auth/application/use-cases/terminate-session'
 
 import { CryptographyModule } from '../cryptography/cryptography.module'
@@ -15,10 +17,12 @@ import { KeyValueModule } from '../key-value/key-value.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { ConfirmEmailController } from './controllers/confirm-email.controller'
 import { ForgotPasswordController } from './controllers/forgot-password.controller'
+import { GetProfileController } from './controllers/get-profile.controller'
 import { LogoutController } from './controllers/logout.controller'
 import { RefreshTokenController } from './controllers/refresh-token.controller'
 import { RegisterController } from './controllers/register.controller'
 import { ResetPasswordController } from './controllers/reset-password.controller'
+import { RevokeAllSessionsController } from './controllers/revoke-all-sessions.controller'
 
 @Module({
   imports: [DatabaseModule, KeyValueModule, CryptographyModule, EnvModule],
@@ -30,6 +34,8 @@ import { ResetPasswordController } from './controllers/reset-password.controller
     LogoutController,
     ForgotPasswordController,
     ResetPasswordController,
+    GetProfileController,
+    RevokeAllSessionsController,
   ],
   providers: [
     EnrollIdentityUseCase,
@@ -39,6 +45,8 @@ import { ResetPasswordController } from './controllers/reset-password.controller
     TerminateSessionUseCase,
     InitiatePasswordRecoveryUseCase,
     ResetPasswordUseCase,
+    RetrieveProfileUseCase,
+    RevokeTokensUseCase,
   ],
 })
 export class HttpModule {}
