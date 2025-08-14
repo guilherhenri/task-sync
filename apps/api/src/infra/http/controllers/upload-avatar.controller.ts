c1e9a5b3ea1b61178c665710cce5345f737b3a0d
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-14 20:30:48 -0300

    feat(infra): implement upload avatar controller
    - Creates the `UploadAvatarController`.
    - This endpoint handles `multipart/form-data` requests, uses custom validators
    for file type and size, orchestrates the `UploadAndUpdateAvatarUseCase`,
    and returns the new avatar URL.

---
+++ apps/api/src/infra/http/controllers/upload-avatar.controller.ts
@@ -0,0 +1,157 @@
+import {
+  BadRequestException,
+  Controller,
+  HttpCode,
+  NotFoundException,
+  ParseFilePipe,
+  Post,
+  UnsupportedMediaTypeException,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+import { ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger'
+import { z } from 'zod/v4'
+
+import { InvalidAvatarTypeError } from '@/domain/auth/application/use-cases/errors/invalid-avatar-type'
+import { ResourceNotFoundError } from '@/domain/auth/application/use-cases/errors/resource-not-found'
+import { UploadAndUpdateAvatarUseCase } from '@/domain/auth/application/use-cases/upload-and-update-avatar'
+import { CurrentUser } from '@/infra/auth/decorators/current-user'
+import type { UserPayload } from '@/infra/auth/types/jwt-payload'
+import { EnvService } from '@/infra/env/env.service'
+
+import {
+  ApiZodBody,
+  ApiZodNotFoundResponse,
+  ApiZodResponse,
+  ApiZodValidationFailedResponse,
+} from '../decorators/zod-openapi'
+import { ApiZodUnsupportedMediaTypeResponse } from '../decorators/zod-openapi/api-zod-unsupported-media-type-response'
+import { JwtUnauthorizedResponse } from '../responses/jwt-unauthorized'
+import {
+  FormattedFileTypeValidator,
+  ReadableMaxFileSizeValidator,
+} from '../validators/file-upload-validators'
+
+const uploadAvatarBodySchema = z.object({
+  file: z.instanceof(File).refine((file) => {
+    const validTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/webp']
+    return validTypes.includes(file.type)
+  }),
+})
+
+const uploadAvatarResponseSchema = z.object({
+  avatar_url: z.string(),
+})
+
+const uploadAvatarBodyDescription: Record<
+  keyof z.infer<typeof uploadAvatarBodySchema>,
+  string
+> = {
+  file: 'Uma imagem de até 2MB nos tipos png, jpg, jpeg ou webp',
+}
+
+@ApiTags('auth')
+@Controller('/upload-avatar')
+export class UploadAvatarController {
+  constructor(
+    private readonly uploadAndUpdateAvatar: UploadAndUpdateAvatarUseCase,
+    private readonly config: EnvService,
+  ) {}
+
+  @Post()
+  @HttpCode(200)
+  @ApiOperation({
+    summary: 'Upload avatar',
+    description: "Upload am image for user's avatar.",
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiZodBody({
+    schema: uploadAvatarBodySchema,
+    description: uploadAvatarBodyDescription,
+  })
+  @ApiZodResponse({
+    status: 200,
+    description: 'Avatar uploaded',
+    schema: uploadAvatarResponseSchema,
+    examples: {
+      avatar_url: '1b25c2e2-9ba2-4fd7-b2a0-28e3bae7d527-1755190078399.jpeg',
+    },
+  })
+  @ApiZodNotFoundResponse({
+    description: 'Resource not found',
+    custom: { message: 'Usuário não encontrado.' },
+  })
+  @ApiZodUnsupportedMediaTypeResponse({ description: 'Invalid media type' })
+  @ApiZodValidationFailedResponse({
+    description: 'Validation Failed',
+    custom: {
+      field: 'file',
+      message: 'O arquivo excede o tamanho máximo permitido de 2MB.',
+    },
+  })
+  @JwtUnauthorizedResponse()
+  @UseInterceptors(FileInterceptor('file'))
+  async handle(
+    @UploadedFile() file: Express.Multer.File,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const parsePipe = new ParseFilePipe({
+      validators: [
+        new ReadableMaxFileSizeValidator({
+          maxSize: 10024 * 1024 * 2, // 2MB
+        }),
+        new FormattedFileTypeValidator({
+          fileType: '.(png|jpg|jpeg|webp)',
+          skipMagicNumbersValidation: this.config.get('NODE_ENV') === 'test',
+        }),
+      ],
+      exceptionFactory: (error) => {
+        const isFileMissing = error.includes('File is required')
+
+        const message = isFileMissing
+          ? 'Nenhum arquivo foi enviado. Por favor, envie um arquivo.'
+          : error
+
+        return new BadRequestException({
+          message,
+          statusCode: 400,
+          errors: {
+            type: 'validation',
+            details: [
+              {
+                filed: 'file',
+                message: isFileMissing ? 'Arquivo é obrigatório.' : message,
+              },
+            ],
+          },
+        })
+      },
+    })
+
+    file = await parsePipe.transform(file)
+
+    const result = await this.uploadAndUpdateAvatar.execute({
+      userId: user.sub,
+      fileName: file.originalname,
+      fileType: file.mimetype,
+      body: file.buffer,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+      switch (error.constructor) {
+        case ResourceNotFoundError:
+          throw new NotFoundException(error.message)
+        case InvalidAvatarTypeError:
+          throw new UnsupportedMediaTypeException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
+    }
+
+    const { avatarUrl } = result.value
+
+    return { avatar_url: avatarUrl }
+  }
+}
