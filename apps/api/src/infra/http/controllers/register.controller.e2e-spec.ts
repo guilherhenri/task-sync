e191ae6f7d5fb3470832d59c591c18b6146ddb8a
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-16 09:41:36 -0300

    test(e2e): add error path tests for register endpoint
    - Expands the E2E test suite for the `/sign-up` endpoint.
    - New tests are added to cover failure scenarios, including invalid input data
    (400 Bad Request) and attempting to register with an email that is already in use
    (409 Conflict).

--- apps/api/src/infra/http/controllers/register.controller.e2e-spec.ts
+++ apps/api/src/infra/http/controllers/register.controller.e2e-spec.ts
@@ -1,23 +1,27 @@
 import type { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
+import { UserFactory } from '@test/factories/make-user'
 import { TestAppModule } from '@test/modules/test-app.module'
 import request from 'supertest'
 
+import { DatabaseModule } from '@/infra/database/database.module'
 import { User } from '@/infra/database/typeorm/entities/user.entity'
 import { TypeOrmService } from '@/infra/database/typeorm/typeorm.service'
 
 describe('Register (E2E)', () => {
   let app: INestApplication
   let typeorm: TypeOrmService
+  let userFactory: UserFactory
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [TestAppModule],
+      imports: [TestAppModule, DatabaseModule],
+      providers: [UserFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
-
     typeorm = moduleRef.get(TypeOrmService)
+    userFactory = moduleRef.get(UserFactory)
 
     await app.init()
   })
@@ -43,5 +47,55 @@ describe('Register (E2E)', () => {
     })
 
     expect(userOnDatabase).toBeTruthy()
+  })
+
+  it('[POST] /sign-up | invalid input data', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/sign-up')
+      .send({
+        email: 'johndoe',
+      })
+      .expect(400)
+
+    expect(response.body).toMatchObject({
+      message: 'Validation failed',
+      statusCode: 400,
+      errors: {
+        type: 'validation',
+        details: [
+          {
+            field: 'name',
+            message: 'O nome é obrigatório.',
+          },
+          {
+            field: 'email',
+            message: 'O e-mail deve ser válido.',
+          },
+          {
+            field: 'password',
+            message: 'A senha é obrigatória.',
+          },
+        ],
+      },
+    })
+  })
+
+  it('[POST] /sign-up | email already in use', async () => {
+    await userFactory.makeTypeOrmUser({ email: 'alreadyinuse@gmail.com' })
+
+    const response = await request(app.getHttpServer())
+      .post('/sign-up')
+      .send({
+        name: 'John Doe',
+        email: 'alreadyinuse@gmail.com',
+        password: '12345Ab@',
+      })
+      .expect(409)
+
+    expect(response.body).toMatchObject({
+      message: `O e-mail "alreadyinuse@gmail.com" já está em uso.`,
+      error: 'Conflict',
+      statusCode: 409,
+    })
   })
 })
