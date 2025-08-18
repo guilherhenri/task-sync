303a9f9b55acffe9e1a24d6d84916de5029cb66c
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-18 19:26:36 -0300

    refactor(infra): register logging interceptors in app module
    - Updates the `AppModule` to globally register `ErrorLoggingInterceptor` and
    `LoggingInterceptor`.
    - This ensures that all incoming HTTP requests and unhandled errors are
    automatically logged.

--- apps/api/src/infra/app.module.ts
+++ apps/api/src/infra/app.module.ts
@@ -1,22 +1,33 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 
 import { AuthModule } from './auth/auth.module'
 import { EnvModule } from './env/env.module'
 import { EventsModule } from './events/events.module'
 import { HttpModule } from './http/http.module'
+import { ErrorLoggingInterceptor } from './logging/interceptors/error-logging.interceptor'
+import { LoggingInterceptor } from './logging/interceptors/logging.interceptor'
+import { LoggingModule } from './logging/logging.module'
 import { WorkersModule } from './workers/workers.module'
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    LoggingModule,
     AuthModule,
     HttpModule,
     EnvModule,
     EventsModule,
     WorkersModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ErrorLoggingInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
   ],
 })
 export class AppModule {}
