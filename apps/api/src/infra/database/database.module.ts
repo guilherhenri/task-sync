5405eb3636637c2cda14ca8185452d967e2dcd7c
Guilherme Henrique <guilherhenri12@gmail.com>
2025-08-27 20:27:35 -0300

    refactor(infra): import observabilitymodule into databasemodule
    - Updates the `DatabaseModule` to import the `ObservabilityModule`.
    - This ensures that repository implementations have access to the `LoggerPort` and
    `MetricsPort` for detailed database-level instrumentation.

--- apps/api/src/infra/database/database.module.ts
+++ apps/api/src/infra/database/database.module.ts
@@ -7,7 +7,7 @@ import { EmailRequestsRepository } from '@/domain/email/application/repositories
 
 import { EnvModule } from '../env/env.module'
 import { EnvService } from '../env/env.service'
-import { MetricsModule } from '../metrics/metrics.module'
+import { ObservabilityModule } from '../observability/observability.module'
 import { mongooseConfig } from './mongoose/mongoose.config'
 import { MongooseService } from './mongoose/mongoose.service'
 import { MongooseEmailRequestsRepository } from './mongoose/repositories/mongoose-email-requests-repository'
@@ -17,7 +17,7 @@ import { TypeOrmService } from './typeorm/typeorm.service'
 
 @Module({
   imports: [
-    MetricsModule,
+    ObservabilityModule,
     TypeOrmModule.forRootAsync({
       imports: [EnvModule],
       useFactory: typeOrmConfig,
